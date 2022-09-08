42a421706dabfeda071375e9b18ba938a33ce14f
Sergey <Sergey@localhost.localdomain>
2022-09-08 20:13:58 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
 import os
+from random import choice
 import telegram
 from dotenv import load_dotenv
 
@@ -6,5 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv('TG_API_KEY')
 channel_id = os.getenv('CHANNEL_ID')
+
 bot = telegram.Bot(token=api_key)
-bot.send_message(chat_id=channel_id, text='Nevermind.')
+bot.send_message(chat_id=channel_id, text='Here comes picture!')
+
+path_to_images = os.path.join(os.getcwd(), 'images')
+random_image = choice(os.listdir(path_to_images))
+with open(os.path.join(path_to_images, random_image), 'rb') as file:
+    bot.send_document(chat_id=channel_id, document=file)
